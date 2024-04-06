c0944c8eb484c6d02ccecae32cd154e88ed57e4d
KENGNI <cedrix.kengni@etu.univ-grenoble-alpes.fr>
2024-04-06 08:02:06 +0200

    findAllByCandidateEvaluationGridEntitiesGradeLessThan échoue

--- server/src/test/java/fr/uga/l3miage/spring/tp3/repositories/CandidateRepositoryTest.java
+++ server/src/test/java/fr/uga/l3miage/spring/tp3/repositories/CandidateRepositoryTest.java
@@ -22,6 +22,8 @@ public class CandidateRepositoryTest {
     private CandidateRepository candidateRepository;
     @Autowired
     private TestCenterRepository testCenterRepository;
+    @Autowired
+    private  CandidateEvaluationGridRepository candidateEvaluationGridRepository;
 
     @Test
     void testRequestfindAllByTestCenterEntityCode(){
@@ -77,7 +79,7 @@ public class CandidateRepositoryTest {
         candidateRepository.save(candidateEntity1);
         candidateRepository.save(candidateEntity2);
 
-        // relation bidirectionnelle sens testCenterEntity--->CandidateEntity
+        // relation bidirectionnelle sens testCenterEntity--->CandidateEntitieS
         testCenterEntity.setCandidateEntities(Set.of(candidateEntity, candidateEntity2)); // je sauvegarde les candidats 1 et 2 comme attributs de testCenterEntity
         testCenterEntity1.setCandidateEntities( Set.of(candidateEntity1));
 
@@ -94,13 +96,120 @@ public class CandidateRepositoryTest {
         //then
         assertThat(candidateEntitiesResponses2).hasSize(0);
         // when
-        Set<CandidateEntity> candidateEntitiesResponses3 = candidateRepository.findAllByTestCenterEntityCode(GRE); // testcenter non présent
+        Set<CandidateEntity> candidateEntitiesResponses3 = candidateRepository.findAllByTestCenterEntityCode(GRE); // 2 testcenter  présents
         //then
         assertThat(candidateEntitiesResponses3).hasSize(2);
         assertThat(candidateEntitiesResponses3.stream().findFirst().get().getTestCenterEntity().getCode()).isEqualTo(GRE);
+    }
+
+
+    @Test
+    void testRequestfindAllByCandidateEvaluationGridEntitiesGradeLessThan(){
+
+        CandidateEvaluationGridEntity candidateEvaluationGridEntity15 = CandidateEvaluationGridEntity
+                .builder()
+                .grade(15)
+                .build();
+
+        CandidateEvaluationGridEntity candidateEvaluationGridEntity5 = CandidateEvaluationGridEntity
+                .builder()
+                .grade(5)
+                .build();
+
+        CandidateEvaluationGridEntity candidateEvaluationGridEntity7_5 = CandidateEvaluationGridEntity
+                .builder()
+                .grade(7.5)
+                .build();
+
+        CandidateEvaluationGridEntity candidateEvaluationGridEntity17 = CandidateEvaluationGridEntity
+                .builder()
+                .grade(17)
+                .build();
+
+        CandidateEvaluationGridEntity candidateEvaluationGridEntity14 = CandidateEvaluationGridEntity
+                .builder()
+                .grade(14)
+                .build();
+
+        CandidateEvaluationGridEntity candidateEvaluationGridEntity8 = CandidateEvaluationGridEntity
+                .builder()
+                .grade(8)
+                .build();
+
+        candidateEvaluationGridRepository.save(candidateEvaluationGridEntity15);
+        candidateEvaluationGridRepository.save(candidateEvaluationGridEntity8);
+        candidateEvaluationGridRepository.save(candidateEvaluationGridEntity7_5);
+        candidateEvaluationGridRepository.save(candidateEvaluationGridEntity17);
+        candidateEvaluationGridRepository.save(candidateEvaluationGridEntity14);
+        candidateEvaluationGridRepository.save(candidateEvaluationGridEntity8);
 
 
 
+
+        CandidateEntity candidateEntity = CandidateEntity
+                .builder() //15 et 5
+                .firstname("CedB")
+                .lastname("KENB")
+                .email("Bblabla@gmail.com")
+                .phoneNumber("62121313")
+                .birthDate(LocalDate.parse("2003-02-15"))
+                .hasExtraTime(Boolean.parseBoolean("False"))
+                .candidateEvaluationGridEntities(Set.of(candidateEvaluationGridEntity15, candidateEvaluationGridEntity8))
+                .build();
+
+        CandidateEntity candidateEntity1 = CandidateEntity
+                .builder()
+                .firstname("MaxB")
+                .lastname("BANB")
+                .email("Bblibli@gmal.com")
+                .phoneNumber("51111111")
+                .birthDate(  LocalDate.parse("1999-01-23"))
+                .hasExtraTime(Boolean.parseBoolean("True"))
+                .candidateEvaluationGridEntities( Set.of(candidateEvaluationGridEntity14, candidateEvaluationGridEntity8, candidateEvaluationGridEntity7_5))
+                .build();
+
+        CandidateEntity candidateEntity2 = CandidateEntity
+                .builder()
+                .firstname("CarlB")
+                .lastname("NENB")
+                .email("Bblabla@yahoo.com")
+                .phoneNumber("54444444")
+                .birthDate( LocalDate.parse("1884-04-17"))
+                .hasExtraTime(Boolean.parseBoolean("False"))
+                .candidateEvaluationGridEntities( Set.of( candidateEvaluationGridEntity17))
+                .build();
+
+        candidateRepository.save(candidateEntity);   // maintenent on enregistre les candidateEntity en base
+        candidateRepository.save(candidateEntity1);
+        candidateRepository.save(candidateEntity2);
+
+        // Relations bidirectionnelles sens CandidateEvaluationGridEntitieS ---> CandidateEntity
+
+        candidateEvaluationGridEntity15.setCandidateEntity(candidateEntity);
+        candidateEvaluationGridEntity8.setCandidateEntity(candidateEntity);
+
+        candidateEvaluationGridEntity14.setCandidateEntity(candidateEntity1);
+        candidateEvaluationGridEntity8.setCandidateEntity(candidateEntity1);
+        candidateEvaluationGridEntity7_5.setCandidateEntity(candidateEntity1);
+
+        candidateEvaluationGridEntity17.setCandidateEntity(candidateEntity2);
+
+
+        // when
+
+        Set<CandidateEntity> candidateEntitiesResponses = candidateRepository.findAllByCandidateEvaluationGridEntitiesGradeLessThan(15);
+        Set<CandidateEntity> candidateEntitiesResponses2 = candidateRepository.findAllByCandidateEvaluationGridEntitiesGradeLessThan(14);
+        Set<CandidateEntity> candidateEntitiesResponses3 = candidateRepository.findAllByCandidateEvaluationGridEntitiesGradeLessThan(17);
+        Set<CandidateEntity> candidateEntitiesResponses4 = candidateRepository.findAllByCandidateEvaluationGridEntitiesGradeLessThan(5);
+
+        //then
+        assertThat(candidateEntitiesResponses).hasSize(2);
+        assertThat(candidateEntitiesResponses2).hasSize(2);
+        assertThat(candidateEntitiesResponses3).hasSize(1);
+        assertThat(candidateEntitiesResponses).hasSize(0);
+
+        // ****Ce test sur la fonction RequestfindAllByCandidateEvaluationGridEntitiesGradeLessThan échoue,
+         // ****celà nous prouve qu'elle ne retourne pas le résultat attendu?
 
 
     }
